964491fd528620b71fd9c16ebcbefd8244ac9a60
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-01-10 15:02:59 +0300

    Initial commit

---
+++ src/main/java/com/github/ajharry69/kyosktest/KyoskTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.ajharry69.kyosktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyoskTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyoskTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ajharry69/kyosktest/KyoskTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ajharry69.kyosktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyoskTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
